183a8bdb72cd98db522bdda8bbbca71040990315
baly <kevin@baldev.it>
2022-07-07 16:20:12 +0200

    Mail project done

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,14 +1,19 @@
-/*
-Mail:
-Chiedi all’utente la sua email,
-controlla che sia nella lista di chi può accedere,
-stampa un messaggio appropriato sull’esito del controllo.
-*/
-
-// estraggo i dati in input
-
 // creo una lista degli invitati
+const attendees = ['kevin@balydev.it', 'sofia@azzolini.it', 'amleto@merli.it', 'admin@hivesup.com']
 
-// effettuo un controllo per verficare che l'utente sia nella lista degli invitati
+document.getElementById('execute').addEventListener('click', function(){
+    // estraggo i dati in input
+    const userEmail = document.getElementById('email').value
 
-// stampo un messaggio per comunicare l'esito della verifica
+    // effettuo un controllo per verficare che l'utente sia nella lista degli invitati
+    // stampo un messaggio per comunicare l'esito della verifica
+    if (userEmail == attendees[0] || userEmail == attendees[1] || userEmail == attendees[2] || userEmail == attendees[3]){
+        document.getElementById('fieldset').classList.add('d-none')
+        document.getElementById('messageBox').classList.add('d-block')
+        document.getElementById('messageBox').innerHTML = '<p class="fs-6 text-success fw-bold">sei presente nella lista degli invitati<p>'
+    } else {
+        document.getElementById('fieldset').classList.add('d-none')
+        document.getElementById('messageBox').classList.add('d-block')
+        document.getElementById('messageBox').innerHTML = '<p class="fs-6 text-danger fw-bold">mi dispiace, non sei presente nella lista degli invitati<p>'
+    }
+});
